f67a85952b28154fa3fb778625b0054369512080
Geziel Carvalho <geziel.natal@gmail.com>
2022-06-21 19:23:09 -0300

    First slice: reservations

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import reservationsSlice from "../features/reservationsSlice";
 
 export const store = configureStore({
     reducer: {
-        // slices
+        reservations: reservationsSlice
     }
 })
 
